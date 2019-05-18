58caeec33b9243568b0845b43637d81965028818
unknown <1356452262@qq.com>
2019-05-18 22:36:16 +0800

    wxapp

---
+++ js/webpack/babel/readme.md
@@ -0,0 +1,19 @@
+- 包管理器
+  npm  node package management
+  yarn 来自于Facebook
+  全局安装与本地安装的区别
+    global  -g 安装到path目录
+    在本地项目中也要安装 node_modules
+
+- babel 编译js
+    es5, es6-es9
+    js更新了新功能 如await
+    typescript java一样的js 静态语言
+    写的js跟运行的js不一样（新的技术）
+    写的不是js，但是最终会编译成js
+
+    - babel-core 负责编译js
+        babel index.js -o index_compiled.js
+        有常用的es6->es5 
+        babel-cli 命令行
+        babel-preset-env 
---
+++ wxapp/2048/pages/index/index.js
@@ -0,0 +1,53 @@
+//index.js
+//获取应用实例
+const app = getApp();
+const GameManager = require('./game_manager.js');
+
+Page({
+  data: {
+    grids: []
+  },
+  gameManager: null,
+  touchStartClientX: 0,     //开始时的坐标
+  touchStartClientY: 0,
+  touchEndClientX: 0,
+  touchEndClientY: 0,
+  onLoad: function () {
+    this.gameManager = new GameManager(4);
+    const grids = this.gameManager.setup();
+    this.setData({
+      grids
+    })
+  },
+  touchStart(event) {
+    // console.log('touchStart');
+    // 触点
+    const touch = event.touches[0];
+    this.touchStartClientX = touch.clientX;
+    this.touchStartClientY = touch.clientY;
+  },
+  touchMove(event) {
+    // console.log('touchMove');
+    const touch = event.touches[0];
+    this.touchEndClientX = touch.clientX;
+    this.touchEndClientY = touch.clientY;
+  },
+  touchEnd(event) {
+    // console.log(event);
+    const touch = event.changedTouches[0];
+    this.touchEndClientX = touch.clientX;
+    this.touchEndClientY = touch.clientY;
+    // console.log('touchEnd');
+    const dx = this.touchEndClientX - this.touchStartClientX;
+    const dy = this.touchEndClientY - this.touchStartClientY;
+    const absDx = Math.abs(dx);
+    const absDy = Math.abs(dy);
+    if (Math.max(absDx, absDy) > 10) {
+      console.log('滑动了');
+      // 判断滑动方向
+      // 0上，2下，1右，3左
+      const direction = absDx > absDy ? (dx > 0 ? 1 : 3) : (dy > 0 ? 2 : 0);
+      this.gameManager.move(direction);
+    }
+  }
+})
