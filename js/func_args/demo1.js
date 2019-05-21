a98ef588df9beab5e5dbeb1a69b316609b076fd1
unknown <1356452262@qq.com>
2019-05-21 21:51:34 +0800

    arguments

---
+++ js/func_args/demo1.js
@@ -0,0 +1,6 @@
+function information(name, sex, money, birthday, color, likes, follow, age) {
+    return `${name} ${sex} ${money} ${birthday} ${color} ${likes} ${follow} ${age}`
+}
+
+let user = { name: 'xb', sex: '男', money: '1个亿', birthday: '2-17', color: 'white', likes: '唱跳rap', follow: 'cxk', age: 22};
+information(user);
