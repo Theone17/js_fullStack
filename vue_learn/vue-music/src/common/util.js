1381270c0cb05cff15f6cf26a00350a06c117527
unknown <1356452262@qq.com>
2019-06-27 14:26:29 +0800

    music

--- vue_learn/vue-music/src/common/util.js
+++ vue_learn/vue-music/src/common/util.js
@@ -9,3 +9,8 @@ export function debounce (func, delay) {
     }, delay)
   }
 }
+export function findIndex (list, song) {
+  return list.findIndex ((item) => {
+    return item.id === song.id
+  })
+}
