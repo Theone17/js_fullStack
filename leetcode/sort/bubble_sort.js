aef2e5001debf9d910ad72c247d76305c01dfd1f
unknown <1356452262@qq.com>
2019-07-12 15:08:29 +0800

    冒泡排序

---
+++ leetcode/sort/bubble_sort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  var len = arr.length;
+  var temp;
+  for (var i = 0; i < len - 1; i++) {
+    for (var j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+				temp = arr[j];
+    		arr[j] = arr[j+1];
+    		arr[j+1] = temp;
+      }
+    }
+    
+  }
+  return arr;
+}
+
+const arr = [8, 2, 5, 9, 7];
+console.log(bubbleSort(arr));
