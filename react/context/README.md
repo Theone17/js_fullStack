df3340ba5f2a8c70eedb9fcc2cb03711e4ac847a
unknown <1356452262@qq.com>
2019-07-15 14:48:10 +0800

    context跨组件数据传递

---
+++ react/context/README.md
@@ -0,0 +1,14 @@
+## context
+ 跨组件之间数据传递
+ this.props 父 -> 孙
+ 父组件 -> 子 -> 孙
+ 被react-router react-redux广泛使用
+ 16.0
+# 问题
+  shouldComponentUpdate
+  尽可能少渲染
+  return true
+  return false 不更新
+  如果中间某个组件 shouldComponentUpdate false 则后代组件不会更新
+
+## context 16
---
+++ react/context/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Context15 from './Context15';
+import Context16 from './Context16';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Context15 />
+      <Context16 />
+    </div>
+  );
+}
+
+export default App;
