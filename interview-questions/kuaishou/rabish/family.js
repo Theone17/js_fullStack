765ad3065d71ddea2a11ec7311dfadc40b249f1f
unknown <1356452262@qq.com>
2019-08-06 14:35:29 +0800

    垃圾回收

---
+++ interview-questions/kuaishou/rabish/family.js
@@ -0,0 +1,20 @@
+function marry(man, woman) {
+  woman.husband = man;
+  man.wife = woman;
+  return {
+    father: man,
+    mather: woman
+  }
+}
+let family = marry({
+  name: 'John'
+},{
+  name: 'Ann'
+});
+
+
+  //          Global
+  //            |
+  //       Object(Family)
+  //     /               \
+  // Object(father) <-> Object(mather)
