246fb24c3fd8f9d1e2f44b44b706efe8ab60bfdc
unknown <1356452262@qq.com>
2019-08-10 20:54:43 +0800

    手写elememt-ui的button

---
+++ vue_learn/element/README.md
@@ -0,0 +1,5 @@
+- 一个框架就好
+- props
+  computed 判断依赖于多个条件时
+  :class="[,{}]" 动态样式
+  $emit('event', ) @event="doSomeEvent" 从内向外传值
---
+++ vue_learn/element/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <el-button type="primary" size="mini" @click="doButtonPressed">主要按钮</el-button>
+    <el-button type="success" size="small" v-on:click="doButtonPressed">成功按钮</el-button>
+    <el-button type="info">信息按钮</el-button>
+  </div>
+</template>
+
+<script>
+import ElButton from './components/ElButton';
+
+export default {
+  name: 'App',
+  components: {
+    ElButton
+  },
+  methods: {
+    doButtonPressed() {
+      console.log('pressed');
+    }
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
